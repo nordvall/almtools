3ebaa38b64773f35bc49cdc264266ea564ab96f4
Mattias <mattias.nordvall@telia.com>
2013-01-07 22:36:42 +0100

    Build: VersionAttributeParser plus tests.

---
+++ ALMTools.Build.Tests/VersionAttributeParserTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ALMTools.Build.Tests
+{
+    [TestClass]
+    public class VersionAttributeParserTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_WhenInvokenWithNull_ArgumentNullExceptionIsThrown()
+        {
+            var parser = new VersionAttributeParser(null);
+        }
+        
+        [TestMethod]
+        public void GetVersion_WhenStreamContainsCorrectAttribute_VersionIsReturned()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.WriteLine("[assembly: AssemblyProduct(\"AnyProduct\")]");
+            writer.WriteLine("[assembly: AssemblyVersion(\"1.0.0.0\")]");
+            writer.WriteLine("[assembly: AssemblyFileVersion(\"2.0.0.0\")]");
+            writer.Flush();
+
+            VersionAttributeParser parser = new VersionAttributeParser(stream);
+
+            Version version = parser.GetVersion("AssemblyVersion");
+            Assert.AreEqual(new Version(1, 0, 0, 0), version);
+
+            Version fileVersion = parser.GetVersion("AssemblyFileVersion");
+            Assert.AreEqual(new Version(2, 0, 0, 0), fileVersion);
+
+        }
+
+        [TestMethod]
+        public void GetVersion_WhenStreamDoesNotContainAttribute_NullIsReturned()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.WriteLine("[assembly: AssemblyProduct(\"AnyProduct\")]");
+            writer.Flush();
+
+            VersionAttributeParser parser = new VersionAttributeParser(stream);
+            Version version = parser.GetVersion("AssemblyVersion");
+
+            Assert.IsNull(version);
+
+        }
+
+        [TestMethod]
+        public void SetVersion_WhenStreamContainsCorrectAttribute_VersionIsUpdated()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.WriteLine("[assembly: AssemblyProduct(\"AnyProduct\")]");
+            writer.WriteLine("[assembly: AssemblyVersion(\"1.0.0.0\")]");
+            writer.WriteLine("[assembly: AssemblyFileVersion(\"2.0.0.0\")]");
+            writer.Flush();
+
+            VersionAttributeParser parser = new VersionAttributeParser(stream);
+
+            Version newVersion = new Version(1, 1, 0, 0);
+            parser.SetVersion("AssemblyVersion", newVersion);
+
+            Version updatedVersion = parser.GetVersion("AssemblyVersion");
+
+            Assert.AreEqual(newVersion, updatedVersion);
+
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void SetVersion_WhenStreamDoesNotContainAttribute_ArgumentExceptionIsThrown()
+        {
+            MemoryStream stream = new MemoryStream();
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.WriteLine("[assembly: AssemblyProduct(\"AnyProduct\")]");
+            writer.Flush();
+
+            VersionAttributeParser parser = new VersionAttributeParser(stream);
+
+            Version newVersion = new Version(1, 1, 0, 0);
+            parser.SetVersion("AssemblyVersion", newVersion);
+        }
+
+        [TestMethod]
+        public void SetVersion_WhenUpdatingToShorterVersion_StreamLengthIsReduced()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+            writer.WriteLine("[assembly: AssemblyVersion(\"1.0.0.255\")]");
+            writer.Flush();
+
+            long oldLength = stream.Length;
+
+            VersionAttributeParser parser = new VersionAttributeParser(stream);
+
+            Version newVersion = new Version(1, 1, 0, 0);
+            parser.SetVersion("AssemblyVersion", newVersion);
+
+            long newLength = stream.Length;
+
+            Assert.IsTrue(newLength < oldLength);
+        }
+    }
+}
---
+++ ALMTools.Build/VersionAttributeParser.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ALMTools.Build
+{
+    public class VersionAttributeParser
+    {
+        private Stream _fileStream;
+
+        public VersionAttributeParser(Stream filestream)
+        {
+            if (filestream == null)
+            {
+                throw new ArgumentNullException("filestream");
+            }
+
+            _fileStream = filestream;
+        }
+
+        public Version GetVersion(string attributeName)
+        {
+            if (string.IsNullOrEmpty(attributeName))
+            {
+                throw new ArgumentNullException("attributeName");
+            }
+
+            string text = GetStreamContents();
+
+            int attributePosition = text.IndexOf(attributeName);
+            if (attributePosition >= 0)
+            {
+                Regex regex = new Regex(@"\(""\d+\.\d+\.\d+\.\d+""\)");
+                Match match = regex.Match(text, attributePosition);
+
+                if (match.Success)
+                {
+                    string versionString = match.Value.Trim('(', '"', ')');
+                    Version version = new Version(versionString);
+                    return version;
+                }
+            }
+
+            return null;
+        }
+
+        public void SetVersion(string attributeName, Version version)
+        {
+            if (string.IsNullOrEmpty(attributeName)) 
+            { 
+                throw new ArgumentNullException("attributeName"); 
+            }
+            if (version == null) 
+            { 
+                throw new ArgumentNullException("version"); 
+            }
+
+            string text = GetStreamContents();
+
+            Regex regex = new Regex(attributeName + @"\(""\d+\.\d+\.\d+\.\d+""\)");
+            Match match = regex.Match(text);
+
+            if (match.Success)
+            {
+                string newText = regex.Replace(text, attributeName + "(\"" + version + "\")");
+                SetStreamContents(newText);
+            }
+            else
+            {
+                throw new ArgumentException("Attribute not found.");
+            }
+        }
+
+        private void SetStreamContents(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            _fileStream.Position = 0;
+            _fileStream.SetLength(bytes.Length);
+            _fileStream.Write(bytes, 0, bytes.Length);
+        }
+
+        private string GetStreamContents()
+        {
+            _fileStream.Position = 0;
+
+            var reader = new StreamReader(_fileStream);
+            string text = reader.ReadToEnd();
+            return text;
+        }
+    }
+}
