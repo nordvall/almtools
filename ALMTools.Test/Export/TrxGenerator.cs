184f41b3cc4a02fa22dcb5153f032a288c8e97e8
Mattias Nordvall <mattiasn@kuai.se>
2013-02-20 21:03:42 +0100

    Beginning of TrxGenerator.

---
+++ ALMTools.Test/Export/TrxGenerator.cs
@@ -0,0 +1,51 @@
+﻿using ALMTools.Test.Import;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ALMTools.Test.Export
+{
+    public class TrxGenerator
+    {
+        private List<IResultParser> _resultFiles;
+
+        public TrxGenerator()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            _resultFiles = new List<IResultParser>();
+        }
+
+        public void AddInputFile(IResultParser result)
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException("result");
+            }
+
+            _resultFiles.Add(result);
+        }
+
+        public XDocument Generate()
+        {
+            XDocument document = GetBaseTrxDocument();
+
+            return document;
+        }
+
+        private static XDocument GetBaseTrxDocument()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream trxStream = assembly.GetManifestResourceStream("ALMTools.Test.Export.BaseTrx.xml");
+            XDocument document = XDocument.Load(trxStream);
+            return document;
+        }
+    }
+}
