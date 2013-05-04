e78e7d0f7e5d81a4f08316ab2f02905f1f7b8358
Mattias <mattias.nordvall@telia.com>
2013-05-04 21:37:45 +0200

    Beginning of HtmlCleaner in Build tools.

---
+++ ALMTools.Build.Tests/HtmlCleanerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ALMTools.Build.Tests
+{
+    [TestClass]
+    public class HtmlCleanerTests
+    {
+        [TestMethod]
+        public void Clean_WhenCalledWithHtmlWithOneComment_CommentIsRemoved()
+        {
+            string inputString = "<html><head><!-- kommentar --></head></html>";
+            var cleaner = new HtmlCleaner();
+            string outputString = cleaner.Clean(inputString);
+
+            Assert.AreEqual("<html><head></head></html>", outputString);
+        }
+
+        [TestMethod]
+        public void Clean_WhenCalledWithHtmlIfBlocks_CommentIsNotRemoved()
+        {
+            string inputString = "<html><head><!--[if lt IE 9]><link rel=\"stylesheet\" href=\"explorer.css\" type=\"text/css\" /><![endif]--></head></html>";
+            var cleaner = new HtmlCleaner();
+            string outputString = cleaner.Clean(inputString);
+
+            Assert.AreEqual(inputString, outputString);
+        }
+
+        [TestMethod]
+        public void Clean_WhenCalledWithHtmlWithMultiLineBreaks_ReplacedBySingleLineBreaks()
+        {
+            string inputString = "<html>\r\n<head>\r\n\r\n</head>\r\n\r\n\r\n</html>";
+            var cleaner = new HtmlCleaner();
+            string outputString = cleaner.Clean(inputString);
+
+            Assert.AreEqual("<html>\r\n<head>\r\n</head>\r\n</html>", outputString);
+        }
+
+        [TestMethod]
+        public void Clean_WhenRowEndWithMultipleSpaces_SpacesAreRemoved()
+        {
+            string inputString = "<html> \r\n  \r\n<head>  \r\n</head>\r\n </html>";
+            var cleaner = new HtmlCleaner();
+            string outputString = cleaner.Clean(inputString);
+
+            Assert.AreEqual("<html>\r\n<head>\r\n</head>\r\n </html>", outputString);
+        }
+
+        [TestMethod]
+        public void Clean_WhenThereAreWhitespaceBetweenTags_SpacesAreRemoved()
+        {
+            string inputString = "<html>   <head>  </head> </html>";
+            var cleaner = new HtmlCleaner();
+            string outputString = cleaner.Clean(inputString);
+
+            Assert.AreEqual("<html><head></head></html>", outputString);
+        }
+    }
+}
---
+++ ALMTools.Build/HtmlCleaner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace ALMTools.Build
+{
+    public class HtmlCleaner
+    {
+        public HtmlCleaner()
+        {
+            HtmlComments = true;
+            WhiteSpaceBetweenTags = true;
+        }
+
+        public string Clean(string inputSource)
+        {
+            string modifiedSource = inputSource;
+
+            StripWhiteSpaceAtEndOfLine(ref modifiedSource);
+
+            if (HtmlComments == true)
+            {
+                StripHtmlComments(ref modifiedSource);
+            }
+
+            if (WhiteSpaceBetweenTags == true)
+            {
+                StripWhiteSpaceBetweenTags(ref modifiedSource);
+            }
+
+            return modifiedSource;
+        }
+
+        private void StripWhiteSpaceAtEndOfLine(ref string modifiedSource)
+        {
+            var whitespaceFinder = new Regex(@" +$", RegexOptions.Multiline);
+            modifiedSource = whitespaceFinder.Replace(modifiedSource, "");
+        }
+
+        private void StripWhiteSpaceBetweenTags(ref string modifiedSource)
+        {
+            var whitespaceFinder = new Regex(@"\>\s+\<", RegexOptions.Multiline);
+
+            modifiedSource = whitespaceFinder.Replace(modifiedSource, @"><");
+        }
+
+        private void StripHtmlComments(ref string modifiedSource)
+        {
+            var htmlCommentFinder = new Regex(@"<!--[^\[].*?[^\]]-->", RegexOptions.Singleline);
+
+            modifiedSource = htmlCommentFinder.Replace(modifiedSource, "");
+        }
+
+        public bool HtmlComments { get; set; }
+
+        public bool WhiteSpaceBetweenTags { get; set; }
+    }
+}
