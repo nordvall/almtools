56b635985aedd7e66f92e42954b81411f44c0fe2
Mattias <mattias.nordvall@telia.com>
2013-08-06 13:41:45 +0200

    New projest: TFS, with helper class for finding out unique build names/numbers.

---
+++ ALMTools.TFS.Tests/BuildNumberCheckerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace ALMTools.TFS.Tests
+{
+    [TestClass]
+    public class BuildNumberCheckerTests
+    {
+        private const string _buildDefinitionName = "BuildDefinition";
+
+        [TestMethod]
+        public void EnsureUniqueBuildNumber_WhenNoMatchingBuildsExistOnServer_SuggestedNameIsReturned()
+        {
+            IBuildServer buildServer = Substitute.For<IBuildServer>();
+            var checker = new BuildNumberChecker(buildServer);
+
+            IBuildDefinition buildDefinition = Substitute.For<IBuildDefinition>();
+            string suggestion = string.Concat(_buildDefinitionName, "_1.0.0.0");
+
+            string result = checker.GetUniqueBuildNumber(buildDefinition, suggestion);
+
+            Assert.AreEqual(suggestion, result);
+        }
+
+        [TestMethod]
+        public void EnsureUniqueBuildNumber_WhenSingleMatchingBuildsExistOnServer_SuggestedNameGetsSuffix()
+        {
+            string suggestion = string.Concat(_buildDefinitionName, "_1.0.0.0");
+
+            IBuildDetail buildDetail = Substitute.For<IBuildDetail>();
+            buildDetail.BuildNumber.Returns(suggestion);
+            
+            IBuildQueryResult queryResult = Substitute.For<IBuildQueryResult>();
+            queryResult.Builds.Returns(new IBuildDetail[] { buildDetail });
+
+            IBuildServer buildServer = Substitute.For<IBuildServer>();
+            buildServer.QueryBuilds(Arg.Any<IBuildDetailSpec>()).Returns(queryResult);
+            
+            var checker = new BuildNumberChecker(buildServer);
+
+            IBuildDefinition buildDefinition = Substitute.For<IBuildDefinition>();
+            string result = checker.GetUniqueBuildNumber(buildDefinition, suggestion);
+
+            Assert.AreEqual(suggestion + "-1", result);
+        }
+
+    }
+}
---
+++ ALMTools.TFS/BuildNumberChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace ALMTools.TFS
+{
+    public class BuildNumberChecker
+    {
+        private IBuildServer _buildServer;
+
+        public BuildNumberChecker(IBuildServer buildServer)
+        {
+            if (buildServer == null)
+            {
+                throw new ArgumentNullException("buildServer");
+            }
+
+            _buildServer = buildServer;
+        }
+
+        public string GetUniqueBuildNumber(IBuildDefinition buildDefinition, string suggestedName)
+        {
+            IBuildDetail[] previousBuilds = GetPreviousBuildsWithSameName(buildDefinition, suggestedName);
+
+            if (previousBuilds == null || previousBuilds.Length == 0)
+            {
+                return suggestedName;
+            }
+            else
+            {
+                string newBuildNumber = string.Format("{0}-{1}", suggestedName, previousBuilds.Count());
+                return newBuildNumber;
+            }
+        }
+
+        private IBuildDetail[] GetPreviousBuildsWithSameName(IBuildDefinition buildDefinition, string suggestedName)
+        {
+            IBuildDetailSpec buildDetailSpec = _buildServer.CreateBuildDetailSpec(buildDefinition);
+            buildDetailSpec.QueryOptions = QueryOptions.None;
+            buildDetailSpec.QueryOrder = BuildQueryOrder.FinishTimeDescending;
+            buildDetailSpec.QueryDeletedOption = QueryDeletedOption.IncludeDeleted;
+            buildDetailSpec.MaxBuildsPerDefinition = 100;
+            buildDetailSpec.BuildNumber = string.Concat(suggestedName, "*");
+
+            IBuildQueryResult queryResult = _buildServer.QueryBuilds(buildDetailSpec);
+
+            return queryResult.Builds;
+        }
+    }
+}
